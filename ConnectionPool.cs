dc287dffa706d9a343b9003ba7181bfbf44406a1
Dimitris Klouvas <dimitris.klouvas@gmail.com>
2015-01-28 01:38:49 +0200

    added dqlite support

--- ConnectionPool.cs
+++ ConnectionPool.cs
@@ -2,9 +2,11 @@
 using System;
 using System.Collections.Generic;
 using System.Data;
+using System.Data.Common;
 using System.Data.Odbc;
 using System.Data.OracleClient;
 using System.Data.SqlClient;
+using System.Data.SQLite;
 using System.Linq;
 using System.Text;
 
@@ -12,7 +14,7 @@ namespace DatabasePool
 {
     static class ConnectionType
     {
-        public const string MYSQL = "Mysql Server";
+        public const string MYSQL = "Mysql";
         public const string SQL = "SQL Server";
         public const string SQLCE = "SQL Server Compact";
         public const string ORACLE = "Oracle";
@@ -31,16 +33,20 @@ namespace DatabasePool
     {
         public string Name;
         public string ServerType;
+
         public string Server;
-        public string ServerMapping;
         public string Database;
-        public string DatabaseMapping;
         public string Username;
-        public string UsernameMapping;
         public string Password;
-        public string PasswordMapping;
         public string IntergratedSecurity;
+
+        public string ServerMapping;
+        public string DatabaseMapping;
+        public string UsernameMapping;
+        public string PasswordMapping;
         public string IntergratedSecurityMapping;
+
+        public string Custom;
         //public string ...
         public override string ToString()
         {
@@ -65,146 +71,24 @@ namespace DatabasePool
             {
                 builder.AppendFormat("{0}={1};", IntergratedSecurityMapping, IntergratedSecurity);
             }
+            if (Custom != null)
+            {
+                builder.AppendFormat(Custom);
+            }
 
             return builder.ToString();
         }
     }
 
-    public class ConnectionPool : Pool<DbConnection>
-    {
-        protected ConnectionPool()
-        {
-        }
-
-        private static ConnectionPool instance;
-
-        public static ConnectionPool getInstance()
-        {
-            if (instance == null)
-            {
-                instance = new ConnectionPool();
-            }
-            return instance;
-        }
-
-        public static List<string> GetAllDatabases(ConnectionInfo con)
-        {
-            var servers = new List<string>();
-
-            if (con.ServerType == ConnectionType.MYSQL)
-            {
-
-            }
-            else if (con.ServerType == ConnectionType.SQL)
-            {
-
-            }
-            else if (con.ServerType == ConnectionType.ORACLE)
-            {
-
-            }
-            else if (con.ServerType == ConnectionType.SQLCE)
-            {
-
-            }
-            else if (con.ServerType == ConnectionType.SQLITE)
-            {
-
-            }
-            return servers;
-        }
-
-        public static List<string> GetAllServers(ConnectionInfo con)
-        {
-            return new List<string>();
-        }
-
-        public void Create(ConnectionInfo con, string name = "")
-        {
-            DbConnection _con = null;
-            if (con.ServerType == ConnectionType.MYSQL)
-            {
-                //παρε τις μη κενες τιμές του ConnectionInfo και βάλ' τες με βαση τις παραμτερους για Mysql 
-                con.ServerMapping = "Server";
-                con.DatabaseMapping = "Database";
-                con.UsernameMapping = "Uid";
-                con.PasswordMapping = "Pwd";
-                //
-                _con.Connection = new MySqlConnection(con.ToString());
-            }
-            else if (con.ServerType == ConnectionType.SQL)
-            {
-                //παρε τις μη κενες τιμές του ConnectionInfo και βάλ' τες με βαση τις παραμτερους για SQL 
-                con.ServerMapping = "Server";
-                con.DatabaseMapping = "Database";
-                con.UsernameMapping = "User Id";
-                con.PasswordMapping = "Password";
-                //
-                _con.Connection = new SqlConnection(con.ToString());
-            }
-            else if (con.ServerType == ConnectionType.SQLCE)
-            {
-                //  _con.DbConnection = new SqlCeConnection();
-            }
-            else if (con.ServerType == ConnectionType.ORACLE)
-            {
-                //παρε τις μη κενες τιμές του ConnectionInfo και βάλ' τες με βαση τις παραμτερους για Oracle 
-                con.ServerMapping = "Server";
-                con.DatabaseMapping = "Data Source";
-                con.UsernameMapping = "User Id";
-                con.PasswordMapping = "Password";
-                con.IntergratedSecurityMapping = "Integrated Security";
-                //
-                _con.Connection = new OracleConnection(con.ToString());
-            }
-            else if (con.ServerType == ConnectionType.SQLITE)
-            {
-                //_con.DbConnection = new SQLLiteConnection();
-            }
-            else if (con.ServerType == ConnectionType.ODBC)
-            {
-                //παρε τις μη κενες τιμές του ConnectionInfo και βάλ' τες με βαση τις παραμτερους για Odbc 
-                con.ServerMapping = "";
-                con.DatabaseMapping = "";
-                //
-                _con.Connection = new OdbcConnection(con.ToString());
-            }
-
-            name = name != String.Empty ? name : con.Name;
-            base.Create(name, _con);
-        }
-
-        public void Test(ConnectionInfo con)
-        {
-            //create connection
-            Create(con, "Test");
-            //get connection
-            using (IDbConnection _con = Checkout("Test").Connection)
-            {
-                _con.Open();
-            }
-            //remove connection
-            Container.Remove(con.Name);
-        }
-
-    }
-
     public class DbConnection
     {
         public IDbConnection Connection;
+        public bool isProcedure;
+        public bool isTransaction;
 
-        public DataTable ExecuteProcedure(string procedureName, Dictionary<string, string> parameters = null)
+        public DataTable Execute(string sql, Dictionary<string, string> parameters = null)
         {
-            return Execute(procedureName, true, parameters);
-        }
-
-        public DataTable ExecuteQuery(string sql, Dictionary<string, string> parameters = null)
-        {
-            return Execute(sql, false, parameters);
-        }
-
-        private DataTable Execute(string sql, bool isProcedure, Dictionary<string, string> parameters = null)
-        {
+            IDbTransaction transaction = null;
             IDbCommand command = Connection.CreateCommand();
             command.CommandText = sql;
 
@@ -217,6 +101,12 @@ namespace DatabasePool
             {
                 command.CommandType = CommandType.StoredProcedure;
             }
+
+            if (isTransaction)
+            {
+                transaction = Connection.BeginTransaction();
+                command.Transaction = transaction;
+            }
             //add parameters
             foreach (var param in parameters)
             {
@@ -226,36 +116,56 @@ namespace DatabasePool
 
                 command.Parameters.Add(parameter);
             }
-            //
-            if (Connection is SqlConnection)
+            //execute query and fill table
+            var table = new DataTable();
+            try
             {
-                return Execute<SqlDataAdapter>(command as SqlCommand);
+                //
+                if (Connection is SqlConnection)
+                {
+                    table = ExecuteAdapter<SqlDataAdapter>(command as SqlCommand);
+                }
+                else if (Connection is MySqlConnection)
+                {
+                    table = ExecuteAdapter<MySqlDataAdapter>(command as MySqlCommand);
+                }
+                //else if (Connection is SQLCEconnection)
+                //{
+                //    return ExecuteAdapter< SQLCEDataAdapter>(command as  SQLCECommand);
+                //}
+                else if (Connection is OracleConnection)
+                {
+                    table = ExecuteAdapter<OracleDataAdapter>(command as OracleCommand);
+                }
+                else if (Connection is SQLiteConnection)
+                {
+                    table = ExecuteAdapter<SQLiteDataAdapter>(command as SQLiteCommand);
+                }
+                else if (Connection is OdbcConnection)
+                {
+                    table = ExecuteAdapter<OdbcDataAdapter>(command as OdbcCommand);
+                }
+
+                if (isTransaction)
+                {
+                    transaction.Commit();
+                    try
+                    {
+                        transaction.Rollback();
+                    }
+                    catch (Exception ex2)
+                    {
+                    }
+                }
             }
-            else if (Connection is MySqlConnection)
+            catch (Exception ex)
             {
-                return Execute<MySqlDataAdapter>(command as MySqlCommand);
-            }
-            //else if (DbConnection is SQLCEconnection)
-            //{
-            //    //  _con = new SqlCeConnection();
-            //}
-            else if (Connection is OracleConnection)
-            {
-                return Execute<OracleDataAdapter>(command as OracleCommand);
-            }
-            //else if (DbConnection is SqliteConnection)
-            //{
-            //    //_con = new SQLLiteConnection();
-            //}
-            else if (Connection is OdbcConnection)
-            {
-                return Execute<OdbcDataAdapter>(command as OdbcCommand);
             }
 
-            return new DataTable();
+            return table;
         }
 
-        private DataTable Execute<T>(IDbCommand command)
+        private DataTable ExecuteAdapter<T>(IDbCommand command)
             where T : IDbDataAdapter, new()
         {
             var table = new DataTable();
@@ -285,4 +195,153 @@ namespace DatabasePool
             return table;
         }
     }
+
+    public class ConnectionPool : Pool<DbConnection>
+    {
+        protected ConnectionPool() { }
+
+        private static ConnectionPool instance;
+
+        public static ConnectionPool getInstance()
+        {
+            if (instance == null)
+            {
+                instance = new ConnectionPool();
+            }
+            return instance;
+        }
+
+        public static List<string> GetAllDatabases(ConnectionInfo con)
+        {
+            var databases = new List<string>();
+
+            if (con.ServerType == ConnectionType.MYSQL)
+            {
+
+            }
+            else if (con.ServerType == ConnectionType.SQL)
+            {
+
+            }
+            else if (con.ServerType == ConnectionType.ORACLE)
+            {
+
+            }
+            else if (con.ServerType == ConnectionType.SQLCE)
+            {
+
+            }
+            else if (con.ServerType == ConnectionType.SQLITE)
+            {
+
+            }
+            return databases;
+        }
+
+        public static List<string> GetAllServers(ConnectionInfo con)
+        {
+            var servers = new List<string>();
+
+            if (con.ServerType == ConnectionType.MYSQL)
+            {
+
+            }
+            else if (con.ServerType == ConnectionType.SQL)
+            {
+
+            }
+            else if (con.ServerType == ConnectionType.ORACLE)
+            {
+
+            }
+            else if (con.ServerType == ConnectionType.SQLCE)
+            {
+
+            }
+            else if (con.ServerType == ConnectionType.SQLITE)
+            {
+
+            }
+            return servers;
+        }
+
+        public void Create(ConnectionInfo con, string name = "")
+        {
+            DbConnection _con = null;
+            if (con.ServerType == ConnectionType.MYSQL)
+            {
+                //παρε τις μη κενες τιμές του ConnectionInfo και βάλ' τες με βαση τις παραμτερους για Mysql 
+                con.ServerMapping = "Server";
+                con.DatabaseMapping = "Database";
+                con.UsernameMapping = "Uid";
+                con.PasswordMapping = "Pwd";
+                //
+                _con.Connection = new MySqlConnection(con.ToString());
+            }
+            else if (con.ServerType == ConnectionType.SQL)
+            {
+                //παρε τις μη κενες τιμές του ConnectionInfo και βάλ' τες με βαση τις παραμτερους για SQL 
+                con.ServerMapping = "Server";
+                con.DatabaseMapping = "Database";
+                con.UsernameMapping = "User Id";
+                con.PasswordMapping = "Password";
+                //
+                _con.Connection = new SqlConnection(con.ToString());
+            }
+            else if (con.ServerType == ConnectionType.SQLCE)
+            {
+                con.DatabaseMapping = "Data Source";
+                con.PasswordMapping = "Password";
+                con.Custom = "Encrypt Database = True;";
+
+                //_con.Connection = new SqlCeConnection();
+            }
+            else if (con.ServerType == ConnectionType.ORACLE)
+            {
+                //παρε τις μη κενες τιμές του ConnectionInfo και βάλ' τες με βαση τις παραμτερους για Oracle 
+                con.ServerMapping = "Server";
+                con.DatabaseMapping = "Data Source";
+                con.UsernameMapping = "User Id";
+                con.PasswordMapping = "Password";
+                con.IntergratedSecurityMapping = "Integrated Security";
+                //
+                _con.Connection = new OracleConnection(con.ToString());
+            }
+            else if (con.ServerType == ConnectionType.SQLITE)
+            {
+                con.DatabaseMapping = "Data Source";
+                con.PasswordMapping = "Password";
+                con.Custom = "Version=3;";
+
+                _con.Connection = new SQLiteConnection(con.ToString());
+            }
+            else if (con.ServerType == ConnectionType.ODBC)
+            {
+                //παρε τις μη κενες τιμές του ConnectionInfo και βάλ' τες με βαση τις παραμτερους για Odbc 
+                con.ServerMapping = "Dsn";
+                con.UsernameMapping = "Uid";
+                con.PasswordMapping = "Pwd";
+                //
+                _con.Connection = new OdbcConnection(con.ToString());
+            }
+
+            name = name != String.Empty ? name : con.Name;
+            base.Create(name, _con);
+        }
+
+        public void Test(ConnectionInfo con)
+        {
+            //create connection
+            Create(con, "Test");
+            //get connection
+            using (IDbConnection _con = Checkout("Test").Connection)
+            {
+                _con.Open();
+            }
+            //remove connection
+            Container.Remove(con.Name);
+        }
+
+    }
+
 }
